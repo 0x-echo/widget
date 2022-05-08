f240869ea0475e3ed66b363e6bdb524c97f080e0
QiongrongJiang <qiongrongjiang@gmail.com>
2022-05-08 10:22:25 -0400

    create wallet item

---
+++ components/wallet-item/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="wallet-item">
+    <img 
+      class="wallet-item__logo"
+      :alt="data.label"
+      :src="data.icon" 
+      :title="data.label">
+    
+    <div
+      class="wallet-item__label">
+      {{ data.label }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss">
+.wallet-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: calc((100% - 15px) / 2);
+  padding: 15px 15px 12px;
+  margin-bottom: 15px;
+  border-radius: 12px;
+  background: white;
+  border: 1px solid #E2E8ED;
+  cursor: pointer;
+  transition: all .3s ease;
+  
+  &.active,
+  &:hover {
+    border-color: $primary;
+    box-shadow: 0 0 4px rgba($primary, .5);
+  }
+  
+  &__logo {
+    width: 24px;
+    height: 24px;
+    object-fit: contain;
+  }
+  
+  &__label {
+    margin-top: 6px;
+    font-size: 14px;
+    color: $text-secondary;
+  }
+}
+</style>
