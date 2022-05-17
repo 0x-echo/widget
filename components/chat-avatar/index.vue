cd58c4548c1bdc7cc42ea4e4a17a6c4de2584293
QiongrongJiang <qiongrongjiang@gmail.com>
2022-05-17 07:58:04 -0400

    add boring avatar

--- components/chat-avatar/index.vue
+++ components/chat-avatar/index.vue
@@ -1,21 +1,28 @@
 <template>
   <el-avatar
-    v-bind="$attrs"
+    class="chat-avatar"
     :alt="alt"
     :title="alt">
-    <img
-      src="@/assets/default-avatar.svg" />
+    <boring-avatar
+      :colors="colors" 
+      :name="alt"
+      size="100%"
+      variant="beam" />
   </el-avatar>
 </template>
 
 <script setup>
 import { ElAvatar } from 'element-plus'
+import BoringAvatar from "vue-boring-avatars"
 
 const props = defineProps({
   alt: {
-    type: String
+    type: String,
+    default: 'Avatar'
   }
 })
+
+const colors = ['#FFAD08', '#EDD75A', '#73B06F', '#0C8F8F', '#405059']
 </script>
 
 <style lang="scss">
