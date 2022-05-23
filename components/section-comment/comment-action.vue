f2c5a47e1e6efd0f334e59b87e55649511f1daf2
Airyland <i@mao.li>
2022-05-23 23:03:46 +0800

    add comment counts animation

--- components/section-comment/comment-action.vue
+++ components/section-comment/comment-action.vue
@@ -13,15 +13,29 @@
       </i>
     </span>
     
-    <span
+    <!-- <span
       class="comment-action__count"
       v-if="count">
       {{ count ? $formatNumber(count) : '' }}
+    </span> -->
+
+    <span
+      class="comment-action__count"
+      ref="commentCountRef"
+      v-show="count <= 9999">
+    </span>
+    
+    <span
+      class="comment-action__count"
+      v-if="count > 9999">
+      {{ $formatNumber(count) }}
     </span>
   </div>
 </template>
 
 <script setup>
+import { Flip } from 'number-flip'
+
 const props = defineProps({
   active: {
     type: Boolean,
@@ -36,6 +50,28 @@ const props = defineProps({
   },
   value: {
     type: String
+  }
+})
+
+let flip
+const commentCountRef = ref(null)
+onMounted(() => {
+  flip = new Flip({
+    node: commentCountRef.value,
+    from: props.count,
+    systemArr: [(props.count < 10 && props.count < 10) ? '&nbsp;' : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
+  })
+})
+
+watch(() => props.count, (val, oldVal) => {
+  if (oldVal <= 9999) {
+    try {
+      flip.flipTo({
+        to: val || ' '
+      }) 
+    } catch (e) {
+      console.log(e)
+    }
   }
 })
 </script>
