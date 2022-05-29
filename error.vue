d2cce08f6e6575e913ef4dcb042ac2205d10351d
QiongrongJiang <qiongrongjiang@gmail.com>
2022-05-29 04:26:45 -0400

    add error page

---
+++ error.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    class="error-page">
+    <div
+      class="error-page__content">
+      <img 
+        class="error-page__image"
+        src="@/assets/error.svg" 
+        alt="Something is wrong">
+      
+      <h1
+        class="error-page__title">
+        Something is wrong
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ElButton } from 'element-plus'
+
+const props = defineProps({
+  error: Object
+})
+</script>
+
+<style lang="scss">
+.error-page {
+  padding: 50px 20px;
+  
+  &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  &__image {
+    width: 100%;
+    max-width: 300px;
+  }
+  
+  &__title {
+    margin-top: 10px;
+    font-size: 20px;
+    text-align: center;
+    text-transform: uppercase;
+  }
+}
+</style>
