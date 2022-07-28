f1cdbcec3f52addfcf14d7839f43e111005dbece
Airyland <i@mao.li>
2022-07-29 00:06:36 +0800

    fix API

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ export default {
       GET_USER_SCREEN_NAME: baseURL + '/api/v1/users/reverse_records',
       GET_USER_INFO: baseURL + '/api/v1/users/info',
       GET_REACTIONS: baseURL + '/api/v1/reactions',
-      SYNC_BALANCE: baseURL + '/api/v1/users/sync_balance',
+      SYNC_BALANCE: baseURL + '/api/v1/modules/wallet-balance',
       TIP: baseURL + '/api/v1/tips',
       GET_TIPS: baseURL + '/api/v1/tips',
       CURRENCY: baseURL + '/api/v1/common/currency'
