fbb94dc51b2e4362515c2c4941b25bcb43135892
Airyland <i@mao.li>
2022-09-02 09:15:15 +0800

    try to add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,11 @@ export default defineNuxtConfig({
       name: 'description',
       content: 'ECHO',
     }],
+    script: [{
+      src: 'https://analytics.0xecho.com/js/plausible.js',
+      'data-domain': 'embed.0xecho.com',
+      defer: true
+    }],
     link: [{
       rel: 'icon',
       href: '/favicon.ico',
