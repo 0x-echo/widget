7442b96a9e15bd788fdb888336080c254e5e143f
QiongrongJiang <qiongrongjiang@gmail.com>
2023-01-29 18:04:05 -0500

    tweaks

--- components/echo-footer/index.vue
+++ components/echo-footer/index.vue
@@ -190,7 +190,7 @@ export default {
   }
 }
 
-@media screen and (max-width: $small-mobile-width - 1px) {
+@media screen and (max-width: #{$small-mobile-width - 1px}) {
   .echo-footer {
     &__settings {
       display: block;
