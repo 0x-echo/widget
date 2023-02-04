3653761690ef359f8bac2a778d2fc2a08c00359d
QiongrongJiang <qiongrongjiang@gmail.com>
2023-02-04 15:25:52 -0500

    tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -10,31 +10,6 @@ import useStore from '~~/store'
 const store = useStore()
 const route = useRoute()
 
-onMounted(() => {
-  if (config['color-theme'] === 'auto') {
-		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      store.setData('env', {
-        colorTheme: 'dark'
-      })
-		} else {
-      store.setData('env', {
-        colorTheme: 'light'
-      })
-    }
-
-		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
-			const newColorScheme = event.matches ? 'dark' : 'light'
-      store.setData('env', {
-        colorTheme: newColorScheme
-      })
-		})
-	} else {
-    store.setData('env', {
-      colorTheme: config['color-theme']
-    })
-	}
-})
-
 const config = computed(() => {
   const query = JSON.parse(JSON.stringify(route.query))
   
@@ -70,6 +45,7 @@ const config = computed(() => {
   
   query.modules = query.modules ? query.modules.split(',') : ['comment', 'like', 'dislike', 'tip']
   
+  // Mirror does not support color-theme:auto
   if (query['target-uri'].includes('mirror.xyz') && (!query['color-theme'] || query['color-theme'] === 'auto')) {
     console.warn('ECHO: Mirror does not support color-theme:auto, set to light instead.')
     query['color-theme'] = 'light'
@@ -77,10 +53,34 @@ const config = computed(() => {
     query['color-theme'] = query['color-theme'] || 'auto'
   }
   
+  // set store.env.colorTheme
+  if (query['color-theme'] === 'auto') {
+		if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      store.setData('env', {
+        colorTheme: 'dark'
+      })
+		} else {
+      store.setData('env', {
+        colorTheme: 'light'
+      })
+    }
+
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+			const newColorScheme = event.matches ? 'dark' : 'light'
+      store.setData('env', {
+        colorTheme: newColorScheme
+      })
+		})
+	} else {
+    store.setData('env', {
+      colorTheme: query['color-theme']
+    })
+	}
+  
   // if render index page individually, but color-theme is dark, set a dark background
   if (!store.env.inIframe) {
 		console.info('env: not in iframe')
-		if (store.env.colorTheme === 'dark') {
+		if (query['color-theme'] === 'dark') {
 			query['dark-theme-bg-color'] = query['dark-theme-bg-color'] || '#0d0f17'
 		}
 	}
