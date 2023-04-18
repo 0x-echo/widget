360256ff00a9cb5bfe2094bd4422f70a996c166b
Airyland <i@mao.li>
2023-04-18 09:40:01 -0400

    particle: filter evm_chain chain

--- compositions/particle.js
+++ compositions/particle.js
@@ -5,36 +5,36 @@ import { particle } from '../config'
 import { ElMessage } from 'element-plus'
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
 const { projectId, clientKey, appId } = particle
 
 const particleLogin = async ({ provider, context: { getAuthMessage } }) => {
-  while(!window.particleAuth || !window.particleProvider) {
+  while (!window.particleAuth || !window.particleProvider) {
     await sleep(1000)
   }
 
   const ParticleNetwork = window.particleAuth.ParticleNetwork
   const ParticleProvider = window.particleProvider.ParticleProvider
 
-	const pn = new ParticleNetwork({
-		projectId,
-		clientKey,
-		appId,
-		chainName: 'Ethereum', //optional: current chain name, default Ethereum.
-		chainId: 1, //optional: current chain id, default 1.
-		wallet: {
-			//optional: by default, the wallet entry is displayed in the bottom right corner of the webpage.
-			displayWalletEntry: false, //show wallet entry when connect particle.
-			// defaultWalletEntryPosition: WalletEntryPosition.BR, //wallet entry position
-			supportChains: [ { id: 1, name: 'Ethereum' } ], // optional: web wallet support chains.
-			customStyle: {} //optional: custom wallet style
-		}
-	})
+  const pn = new ParticleNetwork({
+    projectId,
+    clientKey,
+    appId,
+    chainName: 'Ethereum', //optional: current chain name, default Ethereum.
+    chainId: 1, //optional: current chain id, default 1.
+    wallet: {
+      //optional: by default, the wallet entry is displayed in the bottom right corner of the webpage.
+      displayWalletEntry: false, //show wallet entry when connect particle.
+      // defaultWalletEntryPosition: WalletEntryPosition.BR, //wallet entry position
+      supportChains: [ { id: 1, name: 'Ethereum' } ], // optional: web wallet support chains.
+      customStyle: {} //optional: custom wallet style
+    }
+  })
 
-	const particleProvider = new ParticleProvider(pn.auth)
-	const ethersProvider = new ethers.providers.Web3Provider(particleProvider, 'any')
+  const particleProvider = new ParticleProvider(pn.auth)
+  const ethersProvider = new ethers.providers.Web3Provider(particleProvider, 'any')
 
   // logout first
   if (pn.auth.isLogin()) {
@@ -46,25 +46,24 @@ const particleLogin = async ({ provider, context: { getAuthMessage } }) => {
     }
   }
 
-	try {
-		const rs = await pn.auth.login({
-			preferredAuthType: provider //support facebook,google,twitter,apple,discord,github,twitch,microsoft,linkedin etc.
-		})
-		const info = pn.auth.userInfo()
-		console.log('particle user info', info, info.thirdparty_user_info.user_info)
+  try {
+    const rs = await pn.auth.login({
+      preferredAuthType: provider //support facebook,google,twitter,apple,discord,github,twitch,microsoft,linkedin etc.
+    })
+    const info = pn.auth.userInfo()
+    console.log('particle user info', info, info.thirdparty_user_info.user_info)
+    const matchedWallet = info.wallets.find((one) => one.chain_name === 'evm_chain')
+    const from = matchedWallet.public_address
+    const { message, signKeys } = getAuthMessage('EVM', from)
+    const msg = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(message))
+    const params = [ msg, from ]
+    const method = 'personal_sign'
 
-		const from = info.wallets[0].public_address
-		const { message, signKeys } = getAuthMessage('EVM', from)
-		const msg = ethers.utils.hexlify(ethers.utils.toUtf8Bytes(message))
-		const params = [ msg, from ]
-		const method = 'personal_sign'
-
-		const sign = await particleProvider
-			.request({
-				method,
-				params,
-				from
-			})
+    const sign = await particleProvider.request({
+      method,
+      params,
+      from
+    })
     return [
       from,
       message,
@@ -77,13 +76,13 @@ const particleLogin = async ({ provider, context: { getAuthMessage } }) => {
         login_user_info: info.thirdparty_user_info.user_info
       }
     ]
-	} catch (e) {
-		console.log(e)
-	}
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 export default () => {
-	return {
-		particleLogin
-	}
+  return {
+    particleLogin
+  }
 }
